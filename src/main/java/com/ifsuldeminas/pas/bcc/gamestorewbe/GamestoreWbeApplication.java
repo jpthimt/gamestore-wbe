cb9863f829c0c2fa9bcf85d3d3a1c2f5c9895fd5
jpthimt <joaopedro10thim@gmail.com>
2023-10-05 14:23:44 -0300

    first commit

---
+++ src/main/java/com/ifsuldeminas/pas/bcc/gamestorewbe/GamestoreWbeApplication.java
@@ -0,0 +1,13 @@
+package com.ifsuldeminas.pas.bcc.gamestorewbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamestoreWbeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamestoreWbeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ifsuldeminas/pas/bcc/gamestorewbe/GamestoreWbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifsuldeminas.pas.bcc.gamestorewbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamestoreWbeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
