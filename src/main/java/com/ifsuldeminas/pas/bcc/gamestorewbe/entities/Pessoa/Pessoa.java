f71a85ee96b1fae7a99094ec38192bfe736c509d
Fulvio Stefanine <62194735+fustsiju@users.noreply.github.com>
2023-10-10 16:01:51 -0300

    Update Pessoa.java

--- src/main/java/com/ifsuldeminas/pas/bcc/gamestorewbe/entities/Pessoa/Pessoa.java
+++ src/main/java/com/ifsuldeminas/pas/bcc/gamestorewbe/entities/Pessoa/Pessoa.java
@@ -1,4 +1,50 @@
+import java.util.Date;
 package com.ifsuldeminas.pas.bcc.gamestorewbe.entities.Pessoa;
 
-public class Pessoa extends Funcionario{
+public class Pessoa{
+  private String nome;
+  private String cpf;
+  private String email;
+  private String senha;
+  private date Data_Aniversario;
+  
+public Pessoa(){
+    
+}
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+  public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+  public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+  public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+  public date getData_Aniversario() {
+        return Data_Aniversario;
+    }
+
+    public void setData_Aniversario(String Data_Aniversario) {
+        this.Data_Aniversario = Data_Aniversario;
+    }
 }
