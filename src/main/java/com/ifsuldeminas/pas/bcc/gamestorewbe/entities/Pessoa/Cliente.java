2d628fee8bc252d4d80c98395aaf507faa155122
Fulvio Stefanine <62194735+fustsiju@users.noreply.github.com>
2023-10-10 15:57:13 -0300

    Update Cliente.java

--- src/main/java/com/ifsuldeminas/pas/bcc/gamestorewbe/entities/Pessoa/Cliente.java
+++ src/main/java/com/ifsuldeminas/pas/bcc/gamestorewbe/entities/Pessoa/Cliente.java
@@ -1,5 +1,25 @@
 package com.ifsuldeminas.pas.bcc.gamestorewbe.entities.Pessoa;
 
 public class Cliente extends Pessoa{
-  
+  private int codCliente;
+  private String telefone;
+
+  public Cliente(){
+    
+}
+  public int getCodCliente() {
+        return codCliente;
+    }
+
+    public void setCodCliente(int codCliente) {
+        this.codCliente = codCliente;
+    }
+    
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
