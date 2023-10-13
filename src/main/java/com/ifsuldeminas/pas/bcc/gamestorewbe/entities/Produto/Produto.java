4787d9ac17a656d96f262d7ec8e6dd17d809cea5
Thainá Marini <51173892+ThainaMarini@users.noreply.github.com>
2023-10-13 13:58:44 -0300

    Update Produto.java

--- src/main/java/com/ifsuldeminas/pas/bcc/gamestorewbe/entities/Produto/Produto.java
+++ src/main/java/com/ifsuldeminas/pas/bcc/gamestorewbe/entities/Produto/Produto.java
@@ -1,4 +1,27 @@
 package com.ifsuldeminas.pas.bcc.gamestorewbe.entities.Produto;
 
 public class Produto {
+  private int codProduto;
+  private float valor;
+
+  public Produto(){
+    
+  }
+  
+  public void setCodProduto(int codProduto) {
+        this.codProduto = codProduto;
+  }
+  
+  public String getCodProduto() {
+        return email;
+  }
+
+  public void setValor(float valor) {
+        this.valor = valor;
+  }
+  
+  public String getValor() {
+        return valor;
+  }
+  
 }
