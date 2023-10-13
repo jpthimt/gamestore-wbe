c711464652f176d52a0532ff53627278e07abdac
Thainá Marini <51173892+ThainaMarini@users.noreply.github.com>
2023-10-13 14:33:27 -0300

    Criação dos atributos da classe Dlc

--- src/main/java/com/ifsuldeminas/pas/bcc/gamestorewbe/entities/Produto/Dlc.java
+++ src/main/java/com/ifsuldeminas/pas/bcc/gamestorewbe/entities/Produto/Dlc.java
@@ -1,4 +1,29 @@
 package com.ifsuldeminas.pas.bcc.gamestorewbe.entities.Produto;
 
 public class Dlc extends Produto{
+  private String nome;
+  private Jogo ref_jogo;
+  
+  public Dlc(String nome, Jogo ref_jogo){
+    super();
+    this.nome = nome;
+    this.ref_jogo = ref_jogo;
+  }
+  
+  public void setNome(String nome) {
+      this.nome = nome;
+  }
+
+  public String getNome() {
+      return nome;
+  }
+  
+  public void setRef_jogo(Jogo ref_jogo) {
+      this.ref_jogo = ref_jogo;
+  }
+
+  public Jogo getRef_jogo() {
+      return ref_jogo;
+  }
+  
 }
