f164b78c12cb8969d8d7c31c8a25925dd9a39d17
Thainá Marini <51173892+ThainaMarini@users.noreply.github.com>
2023-10-13 14:38:19 -0300

    Criação novo construtor da classe Produto

--- src/main/java/com/ifsuldeminas/pas/bcc/gamestorewbe/entities/Produto/Produto.java
+++ src/main/java/com/ifsuldeminas/pas/bcc/gamestorewbe/entities/Produto/Produto.java
@@ -5,7 +5,11 @@ public class Produto {
   private float valor;
 
   public Produto(){
-    
+  }
+
+  public Produto(int codProduto, float valor){
+    this.codProduto = codProduto;
+    this.valor = valor;
   }
   
   public void setCodProduto(int codProduto) {
